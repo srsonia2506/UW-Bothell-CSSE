6a4bd70cc40a76f5d41f7267e301a28aa8ee6054
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-04-28 22:05:11 +0800

    Add files via upload

--- CSSSKL 342/Lab 4/Palindrome.cpp
+++ CSSSKL 342/Lab 4/Palindrome.cpp
@@ -1,1 +1,12 @@
-//placeholder
+#include <iostream>
+using namespace std;bool isPalindrome(const string& str, int start, int end) {    if (start >= end)        return true;    if (str[start] != str[end])        return false;    return isPalindrome(str, ++start, --end);}
+
+int main() {
+    string str = "abccba";
+    isPalindrome(str, 0, str.length() - 1) ? "Palindrome" : "Not Palindrome";
+
+    return 0;
+}
+
+
+
