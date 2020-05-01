d969b06198cddbbdfd35112b99334451e9d78842
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-05-02 03:37:01 +0800

    Add files via upload

--- CSS 342/Homework 3/list.h
+++ CSS 342/Homework 3/list.h
@@ -7,6 +7,7 @@
 #include "nodedata.h"
 #include <iostream>
 #include <fstream>
+#include <list>
 using namespace std;
 
 //--------------------------  class List  ------------------------------------
@@ -24,8 +25,9 @@ using namespace std;
 //
 // Note this definition is not a complete class and is not fully documented.
 //----------------------------------------------------------------------------
+template <typename T>
 class List {
-    friend ostream& operator<<(ostream&, const List&);
+    friend ostream& operator<<(ostream&, const List<T>&);
 
 public:
     List();                                     // constructor
@@ -48,5 +50,99 @@ private:
 
     Node* head;               // pointer to first node in list
 };
+
+//----------------------------------------------------------------------------
+// Constructor 
+template <typename T>
+List<T>::List() {
+    head = NULL;
+}
+
+//----------------------------------------------------------------------------
+// isEmpty 
+// check to see if List is empty as defined by a NULL head
+template <typename T>
+bool List<T>::isEmpty() const {
+    return head == NULL;
+}
+
+//----------------------------------------------------------------------------
+// insert 
+// insert an item into list; operator< of the T class
+// has the responsibility for the sorting criteria
+template <typename T>
+bool List<T>::insert(T* dataptr) {
+
+    Node* ptr = new Node;
+    if (ptr == NULL) return false;                 // out of memory, bail
+    ptr->data = dataptr;                           // link the node to data
+
+    // if the list is empty or if the node should be inserted before 
+    // the first node of the list
+    if (isEmpty() || *ptr->data < *head->data) {
+        ptr->next = head;
+        head = ptr;
+    }
+
+    // then check the rest of the list until we find where it belongs 
+    else {
+        Node* current = head->next;          // to walk list
+        Node* previous = head;               // to walk list, lags behind
+
+        // walk until end of the list or found position to insert
+        while (current != NULL && *current->data < *ptr->data) {
+            previous = current;                  // walk to next node
+            current = current->next;
+        }
+
+        // insert new node, link it in
+        ptr->next = current;
+        previous->next = ptr;
+    }
+    return true;
+}
+
+//----------------------------------------------------------------------------
+// buildList 
+// continually insert new items into the list
+template <typename T>
+void List<T>::buildList(ifstream& infile) {
+    T* ptr;
+    bool successfulRead;                            // read good data
+    bool success;                                   // successfully insert
+    for (;;) {
+        ptr = new T;
+        successfulRead = ptr->setData(infile);       // fill the T object
+        if (infile.eof()) {
+            delete ptr;
+            ptr = NULL;
+            break;
+        }
+
+        // insert good data into the list, otherwise ignore it
+        if (successfulRead) {
+            success = insert(ptr);
+        }
+        else {
+            delete ptr;
+            ptr = NULL;
+        }
+        if (!success) break;
+    }
+}
+
+//----------------------------------------------------------------------------
+// operator<<  
+// output operator for class List, print data, 
+// responsibility for output is left to object stored in the list
+template <typename T>
+friend ostream& operator<<(ostream& output, const List<T>& thelist) {
+    typename List<T>::Node* current = thelist.head;
+    while (current != NULL) {
+        output << *current->data;
+        current = current->next;
+    }
+    return output;
+}
 
 #endif
